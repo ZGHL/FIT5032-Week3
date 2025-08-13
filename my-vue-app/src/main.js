09d1f9c4445081cffe508299721cc31ea084ad66
ZGHL <2534169360@qq.com>
2025-08-13 16:36:49 +1000

    finish the activity 1-7

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
-import App from './components/JSON.vue'
+import App from './APP.vue'
 
 createApp(App).mount('#app')
