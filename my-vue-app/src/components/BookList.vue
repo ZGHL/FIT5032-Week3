1295a8e1364c37f11a1e61b115883fdab5ce4c64
ZGHL <2534169360@qq.com>
2025-09-17 11:22:42 +1000

    feat: add BookList to get book that ISBN

--- my-vue-app/src/components/BookList.vue
+++ my-vue-app/src/components/BookList.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 
+import { ref, onMounted } from 'vue'
+import { collection, query, where, getDocs } from "firebase/firestore";
+import db from '../firebase/init.js';
+const books = ref<Array<{ id: string; ISBN: number; name: string }>>([]);
+onMounted(async () => {
+  const q = query(collection(db, "books"), where("ISBN", ">", 1000));
+  const querySnapshot = await getDocs(q);
+  books.value = [];
+  querySnapshot.forEach((doc) => {
+    books.value.push({ id: doc.id, ...(doc.data() as { ISBN: number; name: string }) });
+  });
+});
 </script>
 
 <template>
-
+<div>
+  <h1>Book With ISBN > 1000</h1>
+  <ul>
+    <li v-for="book in books" :key="book.id">
+      {{ book.name }} - ISBN: {{ book.ISBN }}
+    </li>
+  </ul>
+</div>
 </template>
 
 <style scoped>
